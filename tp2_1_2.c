482b02b6a9783a08f5d5096b440900c7e476f56c
Ezequiel <ezequielmartinez2424@gamil.com>
2023-03-23 19:12:17 -0300

    Ejercicio 2.d corregido

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -7,12 +7,9 @@ int main(){
     double vt[N], *pvt=vt;
     for(i = 0;i<N; i++)
     {
-        *(pvt++) =1+rand()%100;
-    }
-    pvt=vt;
-    for(i = 0;i<N; i++)
-    {
+        *pvt =1+rand()%100;
         printf("%f ", *pvt++);
     }
+
     return 0;
 }
